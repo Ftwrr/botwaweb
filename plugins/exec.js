db109827f64f53039029be35178d92b45470e871
Fatur <76021496+Ftwrr@users.noreply.github.com>
2023-03-13 04:36:32 +0000

    exec

---
+++ plugins/exec.js
@@ -0,0 +1,45 @@
+import { format } from "node:util";
+import { createRequire } from 'node:module';
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const require = createRequire(__dirname)
+
+let handler = async (m, _2) => {
+	let { client, usedPrefix, noPrefix, args } = _2
+	let _return
+	let _syntax = ''
+	let _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix
+	try {
+		let i = 15
+		let f = {
+			exports: {}
+		}
+		let exec = new (async () => { }).constructor('print', 'm', 'handler', 'require', 'client', 'db', 'store', 'connection', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument', _text)
+		_return = await exec.call(client, (...args) => {
+			if (--i < 1) return;
+			return client.sendMessage(m.id.remote, format(...args))
+		}, m, handler, require, client, CustomArray, process, args, f, f.exports, [client, _2])
+	} catch (e) {
+		_return = e
+	} finally {
+		client.sendMessage(m.id.remote, _syntax + format(_return))
+	}
+
+};
+handler.help = ['> ', '=> ']
+handler.tags = ['advanced']
+handler.customPrefix = /^=?> /
+handler.command = /(?:)/i
+
+handler.owner = true
+
+export default handler;
+
+class CustomArray extends Array {
+  constructor(...args) {
+    if (typeof args[0] == 'number') return super(Math.min(args[0], 10000))
+    else return super(...args)
+  }
+}
---
+++ plugins/waifu.js
@@ -0,0 +1,18 @@
+import Helper from "../lib/helper.js"
+import wweb from 'whatsapp-web.js'
+const { MessageMedia } = wweb
+
+let rgex = /(maid|waifu|marin-kitagawa|mori-calliope|raiden-shogun|oppai|selfies|uniform|ass|hentai|milf|oral|paizuri|ecchi|ero)/i
+let handler = async (m, { args }) => {
+	const waifu = rgex.test(args[0]) ? args[0].match(rgex)[0] : 'waifu'
+	const response = await fetch(Helper.API('https://api.waifu.im', '/search', { included_tags: waifu, gif: false }))
+    const data = await response.json();
+    if (!data.images[0].url) throw data.detail;
+    m.reply( await MessageMedia.fromUrl(data.images[0].url), false, { caption: data.images[0].source || data.images[0].url } )
+}
+
+handler.help = ['waifu'].map(v => v + ` (tag)`)
+handler.tags = ['internet']
+handler.command = /^(waifu)$/i
+
+export default handler
