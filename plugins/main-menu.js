3970941ecd2ee90dad3341c70684d57b0206779d
Fatur <fatothru@gmail.com>
2023-03-13 20:01:34 +0800

    new file:   plugins/main-menu.js 	modified:   plugins/tools-tiktok.js

---
+++ plugins/main-menu.js
@@ -0,0 +1,66 @@
+import { promises } from 'fs'
+import { join } from 'path'
+import { plugins } from '../lib/plugins.js'
+
+let tags = {
+	'main': 'Main',
+	'internet': 'Internet',
+	'tools': 'Tools',
+	'owner': 'Owner',
+	'advanced': 'Advanced',
+	'': 'No Category',
+}
+
+const defaultMenu = {
+	before: `
+	%name
+	%readmore`.trimStart(),
+	header: '*%category*',
+	body: '%cmd',
+	footer: '',
+	after: ``,
+}
+
+let handler = async (m, { client, usedPrefix: _p, __dirname }) => {
+	let name = m._data.notifyName
+
+	let help = Object.values(plugins).filter(plugin => !plugin.disabled).map(plugin => {
+		return {
+			help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
+			tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
+			prefix: 'customPrefix' in plugin,
+			enabled: !plugin.disabled,
+		}
+	})
+	for (let plugin of help) if (plugin && 'tags' in plugin) for (let tag of plugin.tags) if (!(tag in tags) && tag) tags[tag] = tag
+		let _text = [
+			defaultMenu.before,
+			...Object.keys(tags).map(tag => {
+				return defaultMenu.header.replace(/%category/g, tags[tag]) + '\n' + [
+					...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
+						return menu.help.map(help => {
+							return defaultMenu.body.replace(/%cmd/g, help)
+							.trim()
+						}).join('\n')
+					}),
+					defaultMenu.footer
+					].join('\n')
+			}),
+			defaultMenu.after
+			].join('\n')
+	let replace = {
+		'%': '%',
+		name,
+		readmore: readMore
+	}
+	m.reply((_text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])).trim())
+
+}
+
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = /^(menu|help|\?)$/i
+
+export default handler
+
+const readMore = (String.fromCharCode(8206)).repeat(4001)
--- plugins/tools-tiktok.js
+++ plugins/tools-tiktok.js
@@ -6,9 +6,8 @@ let handler = async (m, { args, client, usedPrefix, command }) => {
     if (!args || !args[0]) throw `Input URL:\n${usedPrefix + command} https://www.tiktok.com/@initokyolagii/video/7189917930761506075`;
     const { desc, create_time, author: { unique_id, nickname }, duration, download: { nowm, wm, music, music_info: { title, author } } }  = await tiktokdl(args[0])
     const url = nowm || wm
-    //m.reply(url)
-    /*if (!url.includes(music)) await */ m.reply( await MessageMedia.fromUrl(url, { unsafeMime: true }))//sock.sendFile(m.chat, url, 'tiktok.mp4', `*${nickname}*\n@${unique_id}\n\n${desc ? desc : ''}`.trim(), m)
-    //sock.sendFile(m.chat, music, 'tiktok.mp3', title, m)
+    if (!url.includes(music)) await m.reply( await MessageMedia.fromUrl(url, { unsafeMime: true }), false, { caption: `*${nickname}*\n@${unique_id}\n\n${desc ? desc : ''}`.trim() })
+    m.reply( await MessageMedia.fromUrl(music))
 }
 
 handler.help = ['tiktok'].map(v => v + ' <url>')
