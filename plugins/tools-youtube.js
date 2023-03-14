6854c38013869bbf3840f4c7f80444932ee863e1
Fatur <76021496+Ftwrr@users.noreply.github.com>
2023-03-14 13:01:51 +0000

    modified:   plugins/tools-youtube.js

--- plugins/tools-youtube.js
+++ plugins/tools-youtube.js
@@ -1,4 +1,4 @@
-import got from 'got';
+//import got from 'got';
 import Helper from "../lib/helper.js"
 import wweb from 'whatsapp-web.js'
 const { MessageMedia } = wweb
@@ -6,7 +6,7 @@ const { MessageMedia } = wweb
 let handler = async (m, { args, client, usedPrefix, command }) => {
     if (!args || !args[0]) throw `Input URL:\n${usedPrefix + command} https://www.youtube.com/watch?v=q6EoRBvdVPQ`;
     const url = Helper.API('https://ytdl.tiodevhost.my.id', '', { url: args[0], filter: "audioandvideo", quality: "highestvideo", contenttype: "video/mp4" })
-    m.reply( await MessageMedia.fromUrl( Buffer.from( await got(url).buffer() ), { unsafeMime: true }))
+    m.reply( await MessageMedia.fromUrl( url/* Buffer.from( await got(url).buffer() */, { unsafeMime: true }))
     //m.reply(url)
 }
 
@@ -14,6 +14,6 @@ handler.help = ['youtube'].map(v => v + ` <url>`)
 handler.tags = ['tools']
 handler.command = /^y(ou)?t(ube)?(v|mp4)?$/i
 
-handler.disabled = true
+//handler.disabled = true
 
 export default handler
