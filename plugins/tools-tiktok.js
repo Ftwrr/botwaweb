fc2aea4e7543fb034967798810a7a8a6a21c3a9b
Fatur <76021496+Ftwrr@users.noreply.github.com>
2023-03-17 02:56:52 +0000

    modified:   plugins/tools-tiktok.js 	modified:   plugins/tools-youtube.js

--- plugins/tools-tiktok.js
+++ plugins/tools-tiktok.js
@@ -3,7 +3,7 @@ import wweb from 'whatsapp-web.js'
 const { MessageMedia } = wweb
 
 let handler = async (m, { args, usedPrefix, command }) => {
-    if (args || args[0]) {
+    if (args) {
         const { desc, create_time, author: { unique_id, nickname }, duration, download: { nowm, wm, music, music_info: { title, author } } } = await tiktokdl(args[0])
         const url = nowm || wm
         if (!url.includes(music)) await m.reply(await MessageMedia.fromUrl(url, { unsafeMime: true }), false, { caption: `*${nickname}*\n@${unique_id}\n\n${desc ? desc : ''}`.trim() })
