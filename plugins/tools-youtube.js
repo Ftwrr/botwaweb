518a34b32b8fc2ceb0db53cf769c092da0438a04
Fatur <76021496+Ftwrr@users.noreply.github.com>
2023-03-18 13:28:58 +0800

    Update tools-youtube.js

--- plugins/tools-youtube.js
+++ plugins/tools-youtube.js
@@ -1,11 +1,12 @@
 import Helper from "../lib/helper.js"
+import got from 'got';
 import wweb from 'whatsapp-web.js'
 const { MessageMedia } = wweb
 
 let handler = async (m, { args, usedPrefix, command }) => {
     if (!args || !args[0]) return m.reply(`Input URL:\n${usedPrefix + command} https://www.youtube.com/watch?v=q6EoRBvdVPQ`);
     const response = Helper.API('https://ytdl.tiodevhost.my.id', '/', { url: args[0], filter: "audioandvideo", quality: "highestvideo", contenttype: "video/mp4" })
-    m.reply(await MessageMedia.fromUrl(response, { unsafeMime: true }))
+    m.reply(await MessageMedia.fromUrl( await got(response).buffer(), { unsafeMime: true }))
 }
 
 handler.help = ['youtube'].map(v => v + ` <url>`)
