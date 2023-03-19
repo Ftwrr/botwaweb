b583ec36b039a0a782ddd0f1989ed79fbd50cb4b
Fatur <76021496+Ftwrr@users.noreply.github.com>
2023-03-19 01:13:39 +0000

    more waifu >///<

--- plugins/internet-waifu.js
+++ plugins/internet-waifu.js
@@ -5,7 +5,7 @@ const { MessageMedia } = wweb
 let rgex = /(maid|waifu|marin-kitagawa|mori-calliope|raiden-shogun|oppai|selfies|uniform|ass|hentai|milf|oral|paizuri|ecchi|ero)/i
 let handler = async (m, { args }) => {
 	const waifu = rgex.test(args[0]) ? args[0].match(rgex)[0] : 'waifu'
-	const response = await fetch(Helper.API('https://api.waifu.im', '/search', { included_tags: waifu, gif: false }))
+	const response = await fetch(Helper.API('https://api.waifu.im', '/search', { included_tags: waifu }))
     const data = await response.json();
     if (!data.images[0].url) return m.reply(data.detail);
     m.reply( await MessageMedia.fromUrl(data.images[0].url), false, { caption: data.images[0].source || data.images[0].url } )
