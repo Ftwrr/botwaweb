d42adb5c34fa6da4792bc32603682ad7c8313407
Fathunnur Suhas <fatothru@gmail.com>
2023-03-22 06:15:37 +0800

    modified:   plugins/tools-nhentai.js

--- plugins/tools-nhentai.js
+++ plugins/tools-nhentai.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (valid) {
         const data = await nhentai.getDoujin(args[0])
         const pdf = await (data.images).PDF()
-        m.reply(new MessageMedia((await fileTypeFromBuffer(pdf)).mime, Buffer.from(pdf).toString("base64"), data.originalTitle + '.pdf' ))
+        m.reply(new MessageMedia((await fileTypeFromBuffer(pdf)).mime, Buffer.from(pdf).toString("base64"), data.title + '.pdf'), null, { caption: data.title } )
     } else return m.reply('Invalid doujin ID')
 }
 
