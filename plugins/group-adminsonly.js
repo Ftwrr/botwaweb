df981a516e7dc6736497dbb29d9fe04943f39fda
Fathunnur Suhas <fatothru@gmail.com>
2023-03-23 07:59:27 +0800

    modified:   plugins/group-adminsonly.js

--- plugins/group-adminsonly.js
+++ plugins/group-adminsonly.js
@@ -1,8 +1,7 @@
 let handler = async (m, { conn, usedPrefix, command, args, text }) => {
 	let chat = await m.getChat();
-	if (chat.announce) {
-		await chat.setMessagesAdminsOnly(false)
-	} else await chat.setMessagesAdminsOnly(true)
+	if (chat.groupMetadata.announce) return await chat.setMessagesAdminsOnly(false)
+	await chat.setMessagesAdminsOnly(true)
 }
 
 handler.help = ['adminonly']
