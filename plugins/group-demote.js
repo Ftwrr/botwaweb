bbdaa7af90e68997d0616c1e3e0da82df81a6d3f
Fathunnur Suhas <fatothru@gmail.com>
2023-03-23 13:15:37 +0800

    modified:   plugins/group-demote.js

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -1,5 +1,6 @@
 let handler = async (m, { conn, usedPrefix, command, args, text }) => {
 	let user = m._data.mentionedJidList
+	if (m.hasQuotedMsg) await user.push(m._data.quotedParticipant)
 	if (!text) return m.reply(`Mention ID:\n${usedPrefix + command} @user`);
 	let chat = await m.getChat();
 	await chat.demoteParticipants(user)
