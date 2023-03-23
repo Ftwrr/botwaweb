7a290e2135d361971ba7200779bd0d56d1775b54
Fathunnur Suhas <fatothru@gmail.com>
2023-03-23 14:20:04 +0800

    modified:   plugins/main-simi.js

--- plugins/main-simi.js
+++ plugins/main-simi.js
@@ -2,13 +2,13 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { text, usedPrefix, command }) => {
 	if (!text) return m.reply(`Input text:\n${usedPrefix + command} halo`);
-	const simiRes = fetch('https://api.simsimi.vn/v1/simtalk', { method: 'post', body: JSON.stringify({ text: text, lc: 'id' }), headers: {'Content-Type': 'application/x-www-form-urlencoded'} })
+	const simiRes = await fetch('https://api.simsimi.vn/v1/simtalk', { method: 'POST', headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, body: `text=${text}&lc=id` })
 	const json = await simiRes.json()
 	m.reply(JSON.stringify(json))
 }
 
 handler.help = ['simsimi'].map(v => v + ' <text>')
 handler.tags = ['main']
-handler.command = /^sim(simi)?$/i
+handler.command = /^simi?(simi)?$/i
 
 export default handler;
