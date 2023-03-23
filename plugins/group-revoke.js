5fc28d48bc04105a0506d8bf8b62fdaac98691ad
Fathunnur Suhas <fatothru@gmail.com>
2023-03-23 15:03:11 +0800

    modified:   plugins/group-revoke.js

--- plugins/group-revoke.js
+++ plugins/group-revoke.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, usedPrefix, command, args, text }) => {
-	let resend = /^resend$/g.test(args[0])
+	let resend = /^(resend|y)$/g.test(args[0])
 	let chat = await m.getChat();
 	await chat.revokeInvite()
 	if (resend) m.reply('https://chat.whatsapp.com/' + await chat.getInviteCode())
 }
 
-handler.help = ['revoke']
+handler.help = ['revoke'].map(v => v + ' (resend)')
 handler.tags = ['group']
 handler.command = /^re(voke|new)(invite|link)?$/i
 
