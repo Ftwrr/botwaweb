b3c79aa05397e7c14f9a7807548f99f48c9c0e1d
Fathunnur Suhas <fatothru@gmail.com>
2023-03-23 15:10:38 +0800

    modified:   plugins/owner-screenshot.js

--- plugins/owner-screenshot.js
+++ plugins/owner-screenshot.js
@@ -3,6 +3,7 @@ import wweb from 'whatsapp-web.js'
 const { MessageMedia } = wweb
 
 let handler = async (m, { conn }) => {
+    await conn.pupPage.setViewport({ width: 720, height: 1600})
     let media = await conn.pupPage.screenshot({fullPage: true})
     m.reply(new MessageMedia((await fileTypeFromBuffer(media)).mime, media.toString("base64")))
 }
