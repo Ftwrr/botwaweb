75007cf368326239e782d9d2ebc3babaf68e5b60
Ftwrr <fatothru@gmail.com>
2023-03-27 04:54:25 +0800

    modified:   handler.js 	modified:   plugins/tools-quote-sticker.js

--- handler.js
+++ handler.js
@@ -150,7 +150,7 @@ async function printMessage(m, conn) {
     console.log(`\n${black(bgGreen('%s'))} from ${black(bgMagenta('~ %s'))} ${black(bgCyan('%s'))} to ${black(bgMagenta('~ %s'))} ${black(bgBlue('%s'))}`,
         m.type,
         contact.name,
-        m.author || m.from,
+        m.from || m.author,
         chat.name,
         chat.id._serialized
         )
