8377da2476a1e545c54de1979f1c48f66c0292da
Ftwrr <fatothru@gmail.com>
2023-03-27 02:39:33 +0800

    modified:   handler.js

--- handler.js
+++ handler.js
@@ -150,7 +150,7 @@ async function printMessage(m, conn) {
     console.log(`\n${black(bgGreen('%s'))} from ${black(bgMagenta('~ %s'))} ${black(bgCyan('%s'))} to ${black(bgMagenta('~ %s'))} ${black(bgBlue('%s'))}`,
         m.type,
         m.fromMe ? conn.info.pushname : contact.name,
-        chat.isGroup ? m.author : m.from,
+        chat.isGroup ? (m.fromMe ? m.from : m.author) : m.from,
         chat.isGroup ? chat.name : chat.name, //TODO get name or pushname from private chat
         chat.id._serialized
         )
