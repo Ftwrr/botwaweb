f9f01b3c7e9cf58f7ee24920439fec56bfc76d31
Ftwrr <fatothru@gmail.com>
2023-03-28 17:10:08 +0800

    modified:   plugins/group-add.js

--- plugins/group-add.js
+++ plugins/group-add.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, command, args, text }) => {
 	if (!text) return m.reply(`Input ID:\n${usedPrefix + command} 1234567891011`);
-	let user = text.split(',').map(v => v.replace(/[^0-9]/g, '') + '@c.us')
+	let user = text.split(',').map(v => (v.replace(/[^0-9]/g, '')).replace(/\D/g, '') + '@c.us')
 	let chat = await m.getChat();
 	await chat.addParticipants(user)
 }
