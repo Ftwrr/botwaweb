3b07120ec1bc764df815085c636ae573e666e00f
Ftwrr <fatothru@gmail.com>
2023-03-28 17:22:46 +0800

    modified:   handler.js

--- handler.js
+++ handler.js
@@ -147,10 +147,10 @@ export async function handler(chatUpdate) {
 async function printMessage(m, conn) {
     const chat = await m.getChat()
     const contact = await m.getContact()
-    // console.log(m)
+    console.log(contact)
     console.log(`\n${black(bgGreen('%s'))} from ${black(bgMagenta('~ %s'))} ${black(bgCyan('%s'))} to ${black(bgMagenta('~ %s'))} ${black(bgBlue('%s'))}`,
         m.type,
-        contact.isMe ? m._data.notifyName : contact.pushname,
+        contact.verifiedName || contact.pushname || m._data.notifyName,
         contact.id._serialized,
         chat.name,
         chat.id._serialized
