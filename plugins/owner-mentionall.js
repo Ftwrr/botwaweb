734d39494d3b4cd1ec41dc091c575e44d1535691
ftwrr <fatothru@gmail.com>
2023-04-01 06:51:54 +0800

    modified:   plugins/main-ping.js 	modified:   plugins/owner-mentionall.js

--- plugins/owner-mentionall.js
+++ plugins/owner-mentionall.js
@@ -1,13 +1,14 @@
 // JamvanHax0r
 
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn, text, usedPrefix, command }) => {
 	const participants = (await m.getChat()).participants.map(v => v.id._serialized);
 	const mentions = await Promise.all(participants.map(jid => conn.getChatById(jid)));
 	let teks = m.hasQuotedMsg ? m._data.quotedMsg.body : text
+	if (!teks) return m.reply(`Input text:\n${usedPrefix + command} hello world`)
 	conn.sendMessage(m.id.remote, teks, { mentions })
 }
 
-handler.help = ['hidetag']
+handler.help = ['hidetag'].map(v => v + ' <text>')
 handler.tags = ['owner']
 handler.command = /^(pengumuman|announce|hiddentag|hidetag)$/i
 
