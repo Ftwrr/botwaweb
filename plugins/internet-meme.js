da7c7da3addd20a258fcd06944131fdde0759ffc
ftwrr <fatothru@gmail.com>
2023-04-01 15:59:19 +0800

    modified:   plugins/internet-meme.js

--- plugins/internet-meme.js
+++ plugins/internet-meme.js
@@ -5,7 +5,7 @@ const { MessageMedia } = wweb
 import fetch from 'node-fetch'
 
 let handler = async (m, { args }) => {
-    const res = await fetch(Helper.API('https://meme-api.com', `/gimme/` + args[0] || ''))
+    const res = await fetch(Helper.API('https://meme-api.com', `/gimme` + args ? `/${args[0]}` : ''))
     await m.reply(Helper.API('https://meme-api.com', `/gimme/` + args[0] || ''))
     if (res.status !== 200) return m.reply(`${res.status} ${res.statusText}`);
     const data = await res.json()
