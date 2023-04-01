434f3dc62f06699ec88ccd85f640b10387e1132d
ftwrr <fatothru@gmail.com>
2023-04-01 15:56:32 +0800

    new file:   plugins/internet-meme.js 	modified:   plugins/internet-waifu.js

---
+++ plugins/internet-meme.js
@@ -0,0 +1,18 @@
+import { fileTypeFromBuffer } from 'file-type';
+import wweb from 'whatsapp-web.js'
+const { MessageMedia } = wweb
+import fetch from 'node-fetch'
+
+let handler = async (m, { args }) => {
+    const res = await fetch(Helper.API('https://meme-api.com', `/gimme/` + args[0] || ''))
+    if (res.status !== 200) return m.reply(`${res.status} ${res.statusText}`);
+    const data = await res.json()
+    const buff = Buffer.from(await (await fetch(data.url)).arrayBuffer())
+    m.reply(new MessageMedia((await fileTypeFromBuffer(buff)).mime, buff.toString("base64")), false, { caption: `` } )
+}
+
+handler.help = ['meme']
+handler.tags = ['internet']
+handler.command = /^(meme|sr|subreddit)$/i
+
+export default handler
