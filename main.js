60a09edbe9a504511600820f20b8af1908b0c7e1
Fatur <76021496+Ftwrr@users.noreply.github.com>
2023-04-25 03:23:07 +0000

    modified:   main.js 	modified:   package.json

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-process.on('uncaughtException', console.error)
-process.on('unhandledRejection', console.error)
+// process.on('uncaughtException', console.error)
+// process.on('unhandledRejection', console.error)
 
 import wweb from 'whatsapp-web.js'
 const { Client, LocalAuth } = wweb
