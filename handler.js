331df6f646463d4c3390bcb4ff7cf77269536b42
Fatur <76021496+Ftwrr@users.noreply.github.com>
2023-05-14 14:18:56 +0800

    to do make welcome leave

--- handler.js
+++ handler.js
@@ -1,71 +1,75 @@
 import Helper from "./lib/helper.js";
-import db, { loadDatabase } from './lib/database.js'
+import db, { loadDatabase } from "./lib/database.js";
 import { plugins } from "./lib/plugins.js";
 import { format } from "node:util";
-import { fileURLToPath } from 'node:url';
+import { fileURLToPath } from "node:url";
 import path from "node:path";
 import etc from "./etc.js";
 import printMessage from "./lib/print.js";
 import { conn } from "./main.js";
 
-
-const isNumber = x => typeof x === 'number' && !isNaN(x)
+const isNumber = (x) => typeof x === "number" && !isNaN(x);
 
 export async function handler(chatUpdate) {
   if (!chatUpdate) return;
   let m = chatUpdate;
   if (!m) return;
-  if (db.data == null)
-    await loadDatabase()
+  if (db.data == null) await loadDatabase();
   try {
     m = m;
     if (!m) return;
 
-    m.sender = (await m.getContact()).id._serialized
-    m.chat = (await m.getChat()).id._serialized
-    m.exp = 0
-    m.limit = false
+    m.sender = (await m.getContact()).id._serialized;
+    m.chat = (await m.getChat()).id._serialized;
+    m.exp = 0;
+    m.limit = false;
 
     try {
-      let user = db.data.users[m.sender]
-      if (typeof user !== 'object')
-        db.data.users[m.sender] = {}
+      let user = db.data.users[m.sender];
+      if (typeof user !== "object") db.data.users[m.sender] = {};
       if (user) {
-        if (!isNumber(user.exp)) user.exp = 0
-        if (!isNumber(user.limit)) user.limit = 10
+        if (!isNumber(user.exp)) user.exp = 0;
+        if (!isNumber(user.limit)) user.limit = 10;
 
-        if (!('banned' in user)) user.banned = false
-        if (!('premium' in user)) user.premium = false
-      } else db.data.users[m.sender] = {
-        exp: 0,
-        limit: 10,
+        if (!("banned" in user)) user.banned = false;
+        if (!("premium" in user)) user.premium = false;
+      } else
+        db.data.users[m.sender] = {
+          exp: 0,
+          limit: 10,
 
-        banned: false,
-        premium: false,
-      }
-      let chat = db.data.chats[m.chat]
-      if (typeof chat !== 'object')
-        db.data.chats[m.chat] = {}
+          banned: false,
+          premium: false,
+        };
+      let chat = db.data.chats[m.chat];
+      if (typeof chat !== "object") db.data.chats[m.chat] = {};
       if (chat) {
-        if (!('isBanned' in chat)) chat.isBanned = false
+        if (!("isBanned" in chat)) chat.isBanned = false;
       } else
         db.data.chats[m.chat] = {
-          isBanned: false
-        }
-      let settings = db.data.settings[conn.info.wid._serialized]
-      if (typeof settings !== 'object') db.data.settings[conn.info.wid._serialized] = {}
+          isBanned: false,
+        };
+      let settings = db.data.settings[conn.info.wid._serialized];
+      if (typeof settings !== "object")
+        db.data.settings[conn.info.wid._serialized] = {};
       if (settings) {
-        if (!('self' in settings)) settings.self = true
-      } else db.data.settings[conn.info.wid._serialized] = {
-        self: true,
-      }
+        if (!("self" in settings)) settings.self = true;
+      } else
+        db.data.settings[conn.info.wid._serialized] = {
+          self: true,
+        };
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
-    if (!m.fromMe && !Helper.isOwner(m) && (db.data.settings[conn.info.wid._serialized]).self) return;
-    m.exp += Math.ceil(Math.random() * 10)
+    if (
+      !m.fromMe &&
+      !Helper.isOwner(m) &&
+      db.data.settings[conn.info.wid._serialized].self
+    )
+      return;
+    m.exp += Math.ceil(Math.random() * 10);
     let usedPrefix;
-    let _user = db.data?.users?.[m.sender]
+    let _user = db.data?.users?.[m.sender];
     const ___dirname = path.join(
       path.dirname(fileURLToPath(import.meta.url)),
       "./plugins"
@@ -90,27 +94,27 @@ export async function handler(chatUpdate) {
       let _prefix = plugin.customPrefix
         ? plugin.customPrefix
         : this.prefix
-          ? this.prefix
-          : Helper.prefix;
+        ? this.prefix
+        : Helper.prefix;
       let match = (
         _prefix instanceof RegExp // RegExp Mode?
           ? [[_prefix.exec(m.body), _prefix]]
           : Array.isArray(_prefix) // Array?
-            ? _prefix.map((p) => {
+          ? _prefix.map((p) => {
               let re =
                 p instanceof RegExp // RegExp in Array?
                   ? p
                   : new RegExp(str2Regex(p));
               return [re.exec(m.body), re];
             })
-            : typeof _prefix === "string" // String?
-              ? [
-                [
-                  new RegExp(str2Regex(_prefix)).exec(m.body),
-                  new RegExp(str2Regex(_prefix)),
-                ],
-              ]
-              : [[[], new RegExp()]]
+          : typeof _prefix === "string" // String?
+          ? [
+              [
+                new RegExp(str2Regex(_prefix)).exec(m.body),
+                new RegExp(str2Regex(_prefix)),
+              ],
+            ]
+          : [[[], new RegExp()]]
       ).find((p) => p[1]);
       if (typeof plugin.before === "function") {
         if (
@@ -137,19 +141,23 @@ export async function handler(chatUpdate) {
           plugin.command instanceof RegExp
             ? plugin.command.test(command)
             : Array.isArray(plugin.command)
-              ? plugin.command.some((cmd) =>
+            ? plugin.command.some((cmd) =>
                 cmd instanceof RegExp ? cmd.test(command) : cmd === command
               )
-              : typeof plugin.command === "string"
-                ? plugin.command === command
-                : false;
+            : typeof plugin.command === "string"
+            ? plugin.command === command
+            : false;
         if (!isAccept) continue;
-        m.plugin = name
+        m.plugin = name;
         if (m.chat in db.data.chats || m.sender in db.data.users) {
-          let chat = db.data.chats[m.chat]
-          let user = db.data.users[m.sender]
-          if (name != 'plugins/owner-ban.js' && (chat?.isBanned || user?.banned) && !Helper.isOwner(m))
-            return
+          let chat = db.data.chats[m.chat];
+          let user = db.data.users[m.sender];
+          if (
+            name != "plugins/owner-ban.js" &&
+            (chat?.isBanned || user?.banned) &&
+            !Helper.isOwner(m)
+          )
+            return;
           // if (name != 'plugins/owner-ban.js' && user?.banned && !Helper.isOwner(m))
           //   return
         }
@@ -158,8 +166,8 @@ export async function handler(chatUpdate) {
           continue;
         }
         if (plugin.premium && !Helper.isPrems(m)) {
-          fail('premium', m)
-          continue
+          fail("premium", m);
+          continue;
         }
         if (plugin.group && !(await Helper.isGroup(m))) {
           fail("group", m);
@@ -176,9 +184,13 @@ export async function handler(chatUpdate) {
           continue;
         }
         m.isCommand = true;
-        let xp = 'exp' in plugin ? parseInt(plugin.exp) : 10
-        m.exp += xp
-        if (!Helper.isPrems(m) && plugin.limit && db.data.users[m.sender].limit < plugin.limit * 1) {
+        let xp = "exp" in plugin ? parseInt(plugin.exp) : 10;
+        m.exp += xp;
+        if (
+          !Helper.isPrems(m) &&
+          plugin.limit &&
+          db.data.users[m.sender].limit < plugin.limit * 1
+        ) {
           fail("limit", m);
           continue;
         }
@@ -220,7 +232,7 @@ export async function handler(chatUpdate) {
               console.error(e);
             }
           }
-          if (m.limit && !Helper.isPrems(m)) m.reply(+m.limit + ' Limits used');
+          if (m.limit && !Helper.isPrems(m)) m.reply(+m.limit + " Limits used");
         }
         break;
       }
@@ -228,13 +240,17 @@ export async function handler(chatUpdate) {
   } catch (e) {
     console.error(e);
   } finally {
-    let user = db.data.stats
+    let user = db.data.stats;
     if (m) {
       if (m.sender && (user = db.data.users[m.sender])) {
-        user.exp += m.exp
-        user.limit -= m.limit * 1
+        user.exp += m.exp;
+        user.limit -= m.limit * 1;
       }
     }
     await printMessage(m, this);
   }
+}
+
+export async function participantsUpdate(notification) {
+  console.log(notification);
 }
--- main.js
+++ main.js
@@ -1,68 +1,78 @@
 // process.on('uncaughtException', console.error)
 // process.on('unhandledRejection', console.error)
 
-import wweb from 'whatsapp-web.js'
-const { Client, LocalAuth } = wweb
-import { handler } from './handler.js'
+import wweb from "whatsapp-web.js";
+const { Client, LocalAuth } = wweb;
+import { handler, participantsUpdate } from "./handler.js";
 import etc from "./etc.js";
-import qrcode from 'qrcode-terminal'
-import { platform } from 'node:os'
+import qrcode from "qrcode-terminal";
+import { platform } from "node:os";
 import {
-    plugins,
-    loadPluginFiles,
-    pluginFolder,
-    pluginFilter
-} from './lib/plugins.js'
-import db from './lib/database.js'
+  plugins,
+  loadPluginFiles,
+  pluginFolder,
+  pluginFilter,
+} from "./lib/plugins.js";
+import db from "./lib/database.js";
 
 const conn = new Client({
-    authStrategy: new LocalAuth({
-        clientId: 'botwaweb',
-        dataPath: './session'
-    }),
-    puppeteer: {
-        args: ['--no-sandbox'],
-        executablePath: platform() === 'win32' ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe' : '/usr/bin/google-chrome-stable'
-    },
-    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15'
+  authStrategy: new LocalAuth({
+    clientId: "botwaweb",
+    dataPath: "./session",
+  }),
+  puppeteer: {
+    args: ["--no-sandbox"],
+    executablePath:
+      platform() === "win32"
+        ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+        : "/usr/bin/google-chrome-stable",
+  },
+  userAgent:
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15",
 });
 
 conn.initialize();
 
-conn.on('loading_screen', (percent, message) => {
-    console.log('LOADING SCREEN', percent, message);
+conn.on("loading_screen", (percent, message) => {
+  console.log("LOADING SCREEN", percent, message);
 });
 
-conn.on('qr', (qr) => {
-    // Generate and scan this code with your phone
-    console.log('QR RECEIVED', qr);
-    qrcode.generate(qr, { small: true });
+conn.on("qr", (qr) => {
+  // Generate and scan this code with your phone
+  console.log("QR RECEIVED", qr);
+  qrcode.generate(qr, { small: true });
 });
 
-conn.on('authenticated', () => {
-    console.log('AUTHENTICATED');
+conn.on("authenticated", () => {
+  console.log("AUTHENTICATED");
 });
 
-conn.on('auth_failure', msg => {
-    // Fired if session restore was unsuccessful
-    console.error('AUTHENTICATION FAILURE', msg);
+conn.on("auth_failure", (msg) => {
+  // Fired if session restore was unsuccessful
+  console.error("AUTHENTICATION FAILURE", msg);
 });
 
-conn.on('ready', async () => {
-    console.log('READY');
-    await conn.sendMessage(etc.owner + "@c.us", `${JSON.stringify(conn.info)}`)
+conn.on("ready", async () => {
+  console.log("READY");
+  await conn.sendMessage(etc.owner + "@c.us", `${JSON.stringify(conn.info)}`);
 });
 
-conn.on('message_create', handler.bind(conn));
+conn.on("message_create", handler.bind(conn));
 
+conn.on(("group_join", "group_leave"), participantsUpdate.bind(conn));
 
 // load plugins
-loadPluginFiles(pluginFolder, pluginFilter, { logger: console, recursiveRead: false }).then(_ => console.log(Object.keys(plugins))).catch(console.error)
+loadPluginFiles(pluginFolder, pluginFilter, {
+  logger: console,
+  recursiveRead: false,
+})
+  .then((_) => console.log(Object.keys(plugins)))
+  .catch(console.error);
 
 setInterval(async () => {
-    await Promise.allSettled([
-        db.data ? db.write() : Promise.reject('db.data is null')
-    ])
-}, 60 * 1000)
+  await Promise.allSettled([
+    db.data ? db.write() : Promise.reject("db.data is null"),
+  ]);
+}, 60 * 1000);
 
-export { conn }
+export { conn };
