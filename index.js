79b0b9917b93594c876831c9f99ec70e2fe025ff
Fatur <76021496+Ftwrr@users.noreply.github.com>
2023-05-16 13:43:26 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
-import cluster from 'node:cluster';
+import { join, dirname } from "path";
+import { createRequire } from "module";
+import { fileURLToPath } from "url";
+import { setupPrimary, fork } from "cluster";
+import { watchFile, unwatchFile } from "fs";
+import { createInterface } from "readline";
 
-cluster.setupPrimary({
-    exec: 'main.js',
-    // args: args.slice(1)
-});
-// console.log(process.argv.slice(2)[0]);
-if (cluster.isPrimary) {
-    const worker = cluster.fork();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const require = createRequire(__dirname); // Bring in the ability to create the 'require' method
+const { name, author } = require(join(__dirname, "./package.json")); // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
+const rl = createInterface(process.stdin, process.stdout);
+
+console.log(`'${name}' By @${author.name || author}`);
+var isRunning = false;
+/**
+ * Start a js file
+ * @param {String} file `path/to/file`
+ */
+function start(file) {
+  if (isRunning) return;
+  isRunning = true;
+  let args = [join(__dirname, file), ...process.argv.slice(2)];
+  setupPrimary({
+    exec: args[0],
+    args: args.slice(1),
+  });
+  let p = fork();
+  p.on("message", (data) => {
+    console.log("[RECEIVED]", data);
+    switch (data) {
+      case "reset":
+        p.process.kill();
+        isRunning = false;
+        start.apply(this, arguments);
+        break;
+      case "uptime":
+        p.send(process.uptime());
+        break;
+    }
+  });
+  p.on("exit", (_, code) => {
+    isRunning = false;
+    console.error("Exited with code:", code);
+    if (code === 0) return;
+    watchFile(args[0], () => {
+      unwatchFile(args[0]);
+      start(file);
+    });
+  });
+  if (!rl.listenerCount())
+    rl.on("line", (line) => {
+      p.emit("message", line.trim());
+    });
 }
+
+start("main.js");
