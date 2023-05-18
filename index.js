d4f8834a8cbcc9415bb29cce284505f1dae81ea5
Fatur <76021496+Ftwrr@users.noreply.github.com>
2023-05-18 10:28:42 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,52 +1,45 @@
+console.log("Starting...");
+
 import { join, dirname } from "path";
 import { createRequire } from "module";
 import { fileURLToPath } from "url";
-import { setupPrimary, fork } from "cluster";
+import cluster, { setupPrimary, fork } from "node:cluster";
+import { watchFile, unwatchFile } from "fs";
 import { createInterface } from "readline";
 
+// https://stackoverflow.com/a/50052194
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname); // Bring in the ability to create the 'require' method
 const { name, author } = require(join(__dirname, "./package.json")); // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
 const rl = createInterface(process.stdin, process.stdout);
+const file = "main.js";
 
 console.log(`'${name}' By @${author.name || author}`);
-var isRunning = false;
-/**
- * Start a js file
- * @param {String} file `path/to/file`
- */
-function start(file) {
-  if (isRunning) return;
-  isRunning = true;
-  let args = [join(__dirname, file), ...process.argv.slice(2)];
-  setupPrimary({
-    exec: args[0],
-    args: args.slice(1),
-  });
-  let p = fork();
-  p.on("message", (data) => {
+
+let args = [join(__dirname, file), ...process.argv.slice(2)];
+console.log([process.argv[0], ...args].join(" "));
+setupPrimary({
+  exec: args[0],
+  args: args.slice(1),
+});
+if (cluster.isPrimary) {
+  const worker = fork();
+  worker.on("message", (data) => {
     console.log("[RECEIVED]", data);
     switch (data) {
       case "reset":
-        console.log("reset bos");
-        p.process.kill();
-        isRunning = false;
-        start.apply(this, arguments);
-        break;
-      case "uptime":
-        p.send(process.uptime());
-        break;
+        worker.process.kill();
+        fork();
     }
   });
-  p.on("exit", (_, code) => {
-    isRunning = false;
+  worker.on("exit", (_, code) => {
     console.error("Exited with code:", code);
-    if (code !== "SIGTERM") start(file);
   });
   if (!rl.listenerCount())
     rl.on("line", (line) => {
-      p.emit("message", line.trim());
+      worker.emit("message", line.trim());
     });
+  setInterval(() => {
+    worker.emit("message", "reset");
+  }, 6 * 60 * 60 * 1000);
 }
-
-start("main.js");
