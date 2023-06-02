84b3e094c09f3cdb8ea30f06745531577b5f7729
Fatur <76021496+Ftwrr@users.noreply.github.com>
2023-06-03 07:16:10 +0800

    Update internet-simi.js

--- plugins/internet-simi.js
+++ plugins/internet-simi.js
@@ -1,12 +1,11 @@
 import fetch from 'node-fetch'
-import Helper from "../lib/helper.js"
 
 let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) return m.reply(`Input text:\n${usedPrefix + command} halo`);
-    const res = await fetch(Helper.API('https://api.itsrose.site', '/others/simi', { text, level: Math.floor(Math.random() * (1000 * 90)) + (1000 * 10), lc: 'id' }))
+	if (!text) return m.reply(`Input text:\n${usedPrefix + command} halo`);
+    const res = await fetch('https://api.simsimi.vn/v2/simtalk', { method: 'POST', headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, body: `text=${text}&lc=id` })
     if (res.status !== 200) return m.reply(`${res.status} ${res.statusText}`);
     const json = await res.json()
-    m.reply(json.result.simi.original)
+    m.reply(json.message)
 }
 
 handler.help = ['simsimi'].map(v => v + ' <text>')
